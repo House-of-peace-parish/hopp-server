b6bb1f5d6be92f9eb0609e4a9265e2c2a3698e18
Your Name <Yoshimitsu247Kakarot@gmail.com>
2025-11-03 08:03:11 -0600

    remodify firebase json file

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
-const serviceAccount = require('../../hopp-b07e0-firebase-adminsdk-fbsvc-66e8c70d5c.json')
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_CLIENT_AUTH_URI,
+  token_uri: process.env.FIREBASE_CLIENT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_CLIENT_AUTH_CERT_URI,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URI,
+  universe_domain: process.env.FIREBASE_CLIENT_UNIVERSE_DOMAIN,
+}
 
 const app = initializeApp({
   credential: cert(serviceAccount),
